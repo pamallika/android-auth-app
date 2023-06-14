97186e1054e6d5c5d1f17403c17849d20d6c3ee6
Alexandr Fedorenko <sasha.fedorenko4736@gmail.com>
2023-06-14 03:13:31 +0300

    add sign and register methods

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,166 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.Objects;
+
+import Models.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRegister, btnSign;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+    RelativeLayout root;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSign = findViewById(R.id.btnSign);
+
+        root = findViewById(R.id.root_layout);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignWindow();
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterWindow();
+            }
+        });
+    }
+
+    private void showSignWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Войти");
+        dialog.setMessage("Введите данные");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View signWindow = inflater.inflate(R.layout.sign_layout, null);
+        dialog.setView(signWindow);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                final MaterialEditText email = signWindow.findViewById(R.id.loginInput);
+                final MaterialEditText password = signWindow.findViewById(R.id.passwordInput);
+                final String emailText = Objects.requireNonNull(email.getText()).toString();
+                final String passwordText = Objects.requireNonNull(password.getText()).toString();
+                if(TextUtils.isEmpty(email.toString())) {
+                    Snackbar.make(root, "Введите логин", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(
+                        passwordText.length() < 5
+                ) {
+                    Snackbar.make(root, "Введите пароль (не менее 5 символов)", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.signInWithEmailAndPassword(emailText,passwordText)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(MainActivity.this, MapActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(root, "Ошибка авторизации " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+    }
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Введите данные");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.register_layout, null);
+        dialog.setView(registerWindow);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setPositiveButton("Сохранить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+            final MaterialEditText email = registerWindow.findViewById(R.id.loginInput);
+            final MaterialEditText password = registerWindow.findViewById(R.id.passwordInput);
+            final String emailText = Objects.requireNonNull(email.getText()).toString();
+            final String passwordText = Objects.requireNonNull(password.getText()).toString();
+                if(TextUtils.isEmpty(email.toString())) {
+                    Snackbar.make(root, "Введите логин", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(
+                        passwordText.length() < 5
+                ) {
+                    Snackbar.make(root, "Введите пароль (не менее 5 символов)", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(emailText, passwordText)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User("", emailText, passwordText);
+
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Snackbar.make(root, "Пользователь добавлен", Snackbar.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+            }
+        });
+        dialog.show();
     }
 }
